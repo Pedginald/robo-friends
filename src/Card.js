fb84c548fb35bed1ff927824c9d956df7ccd4795
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-03-25 11:11:42 -0400

    Adding destructured variables to the card values

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow tc bw2 shadow-5'>
-            <img alt='robots' src='https://robohash.org/test?200x200' />
+            <img alt='robots' src={`https://robohash.org/${id}?test?200x200`} />
             <div>
-                <h2>John Doe</h2>
-                <p>john.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
